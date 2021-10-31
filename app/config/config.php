7308f0ca9d3c842f3c0b6979991d2f8a7f2b552f
Robert W. Cassimiro A. Pinto <44538201+robertcassi@users.noreply.github.com>
2021-10-31 00:48:29 -0300

    update config

--- app/config/config.php
+++ app/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-echo $config['base_url'] = 'http://'.$_SERVER["HTTP_HOST"];
+
+$protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === 0 ? 'https://' : 'http://';
+$config['base_url'] = $protocol . $_SERVER["HTTP_HOST"];
 
 /*
 |--------------------------------------------------------------------------
